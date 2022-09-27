4fb1aab32e65d26b7d6cf12e53b67f5ee1f31162
Zoraez <82158378+ZI-Youtuber@users.noreply.github.com>
2022-09-27 19:45:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Basics
+# print("Hello Replit")
+# print("I am a Coder")
+# print("I am Coding in Python btw :) ")
+
+# print("""We are coding this really long sentence and we are making this for the 100 days of learning python and it is really fun !! """)
+
+
+
+
+# Errors
+# Print("What could go wrong")
+# print "soemthing is wrong"
+# print(Please work)
+
+# # Fixing the errors
+# print("= MUSIC+ =")
+# print("> Songs")
+# print("> Albums")
+# print("> Artists")
+
+# Challenge
+
+print("Zoraez Imran")
+print("27 Sept 2022")
+
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+
+print("I am feeling :) ")
+
+print("You can follow my progress at replit.com/@ZIYoutuber")
